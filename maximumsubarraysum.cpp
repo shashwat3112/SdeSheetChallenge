43f29b8d5bee2ebe8276558a8403a78cbdda6c16
Shashwat Gaurav <53408829+shashwat3112@users.noreply.github.com>
2022-06-22 17:49:55 +0530

    Create maximumsubarraysum.cpp

---
+++ maximumsubarraysum.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+    /*
+        Don't write main().
+        Don't read input, it is passed as function argument.    
+        No need to print anything.
+        Taking input and printing output is handled automatically.
+    */
+    long long int sum=0;
+    long long int maxi = INT_MIN;
+    for(long long int i=0;i<n;i++)
+    {
+        sum= sum + arr[i];
+        maxi = max(sum,maxi);
+        if(sum<0){
+            sum=0;
+        }
+        
+    }
+    if(maxi <0)
+        return 0;
+    return maxi;
+}
